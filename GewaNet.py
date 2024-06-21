69d4f29bd9d079ef64621b50d45e1ded1c03ebb3
yusufSLCN <yusuf.salcan@hotmail.com>
2024-06-21 16:52:25 +0200

    add Gewanet

---
+++ GewaNet.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn as nn
+from torch_geometric.nn import MLP
+from GraspNet import SAModule, GlobalSAModule
+
+class EdgeGraspPredictor(nn.Module):
+    def __init__(self, scene_feat_dim, point_feat_dim=256, out_size=9):
+        super(EdgeGraspPredictor, self).__init__()
+
+        self.predictor = nn.Sequential(
+            nn.Linear(scene_feat_dim + point_feat_dim, scene_feat_dim  // 2),
+            nn.ReLU(),
+            nn.Linear(scene_feat_dim // 2, scene_feat_dim // 4),
+            nn.ReLU(),
+            nn.Linear(scene_feat_dim // 4, out_size),
+        )
+    def forward(self, edge_features):
+        # expanded_querry_point_feat = querry_point_feat[point_batch]
+        # h = torch.cat([edge_features, expanded_querry_point_feat], dim=1)
+        h = self.predictor(edge_features)
+        return h
+
+class Encoder(torch.nn.Module):
+    def __init__(self, out_channels, point_feat_dim=256, ratios=[1.0, 1.0]):
+        super().__init__()
+
+        self.sa1_module = SAModule(ratios[0], 0.2, MLP([3, 64, 64, 128]))
+        self.sa2_module = SAModule(ratios[1], 0.4, MLP([128 + 3, 128, 128, point_feat_dim]))
+        self.sa3_module = GlobalSAModule(MLP([point_feat_dim + 3, point_feat_dim, 512, out_channels]))
+
+    def forward(self, x, pos, batch, query_point_idx):
+        sa1_out = self.sa1_module(x, pos, batch)
+        sa2_out = self.sa2_module(*sa1_out)
+        point_feat, _, point_batch = sa2_out
+        sa3_out = self.sa3_module(*sa2_out)
+        # expanded_scene_feat = sa3_out[0][point_batch]
+        scene_feat = sa3_out[0]
+        point_feat = point_feat[query_point_idx]
+
+        # create edge features by concatenating the scene features with the point features 
+        edge_feat = torch.cat([point_feat, scene_feat], dim=1)
+        x, pos, batch = sa3_out
+
+        return x, edge_feat, point_batch
+    
+class GewaNet(nn.Module):
+    def __init__(self, scene_feat_dim=1024, point_feat_dim=256, predictor_out_size = 9):
+        super(GewaNet, self).__init__()
+        self.encoder = Encoder(out_channels = scene_feat_dim, point_feat_dim=point_feat_dim)
+        self.predictor = EdgeGraspPredictor(scene_feat_dim, point_feat_dim=point_feat_dim, out_size=predictor_out_size)
+    def forward(self, data):
+        vertices, grasp_gt, batch_idx, batch_querry_point_idx = self.collate_fn(data)
+        x, edge_feat, point_batch = self.encoder(None, vertices, batch_idx, batch_querry_point_idx)
+        output = self.predictor(edge_feat)
+        trans_m = self.calculateTransformationMatrix(output)
+        grasp = trans_m.view(-1, 16)
+        return grasp
+    
+    def collate_fn(self, batch):
+        batch_idx = []
+        vertices = []
+        grasp_gt = []
+        batch_querry_point_idx = []
+        vertex_count = 0
+        for i, (points, gt, info) in enumerate(batch):
+            batch_querry_point_idx.append(info['query_point_idx'] + vertex_count)
+            vertex_count = points.shape[0]
+            batch_idx.extend([i] * vertex_count)
+            vertices.append(points)
+            grasp_gt.append(gt)
+                
+        batch_idx = torch.tensor(batch_idx, dtype=torch.int64)
+        vertices = torch.cat(vertices, dim=0)
+        grasp_gt = torch.stack(grasp_gt, dim=0)
+        batch_querry_point_idx = torch.tensor(batch_querry_point_idx, dtype=torch.int64)
+
+        return vertices, grasp_gt, batch_idx, batch_querry_point_idx
+    
+    def calculateTransformationMatrix(self, grasp):
+        translation = grasp[:, :3]
+        r1 = grasp[:, 3:6]
+        r2 = grasp[:, 6:]
+        #orthogonalize the rotation vectors
+        r1 = r1 / torch.norm(r1, dim=1, keepdim=True)
+        r2 = r2 - torch.sum(r1 * r2, dim=1, keepdim=True) * r1
+        r2 = r2 / torch.norm(r2, dim=1, keepdim=True)
+        r3 = torch.cross(r1, r2, dim=1)
+        #create the rotation matrix
+        r = torch.stack([r1, r2, r3], dim=2)
+        #create 4x4 transformation matrix for each 
+        trans_m = torch.eye(4).repeat(len(grasp), 1, 1).to(grasp.device)
+        trans_m[:,:3, :3] = r
+        trans_m[:, :3, 3] = translation
+        return trans_m
+    
+
+if __name__ == "__main__":
+    model = GewaNet(scene_feat_dim= 1024, point_feat_dim=256, predictor_out_size=9) 
+
+    num_meshes = 2
+    mesh_vertex_count = 50
+    total_vertex_count = num_meshes * mesh_vertex_count
+    data = torch.randn((total_vertex_count, 3))
+    pos = torch.randn((total_vertex_count, 3))
+    batch = torch.arange(total_vertex_count, dtype=torch.long)
+    query_point_idx = torch.zeros(num_meshes, dtype=torch.long)
+    for i in range(num_meshes):
+        batch[i*mesh_vertex_count:(i+1)*mesh_vertex_count] = i
+        query_point_idx[i] = i * mesh_vertex_count
+
+    grasps = model(None, pos, batch, query_point_idx)
---
+++ tg_loader.py
@@ -0,0 +1,23 @@
+from acronym_dataset import AcronymDataset
+from torch_geometric.loader import DataListLoader
+from torch_geometric.nn import DataParallel
+from GewaNet import GewaNet
+from create_dataset_paths import save_split_meshes
+import torch
+
+if __name__ == "__main__":
+    train_dataset, val_dataset = save_split_meshes('../data', 100)
+    train_dataset = AcronymDataset(train_dataset)
+    train_loader = DataListLoader(train_dataset, batch_size=2, shuffle=True)
+    model = GewaNet(scene_feat_dim= 1024, point_feat_dim=256, predictor_out_size=9)
+
+    if torch.cuda.device_count() > 1:
+        model = DataParallel(model)
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+    for data in train_loader:
+        # print(len(data))
+        # print(data[0][-1])
+        grasp_pred = model(data)
+        break
